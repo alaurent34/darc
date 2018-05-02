08520a934da570aceadd1e25a3c8189f79311095
Antoine Laurent <antoine@famille-laurent.com>
2018-05-02 10:17:25 -0400

    tool-kamei: translate comment

--- PWS-archive/Tool-python/tool-kamei.py
+++ PWS-archive/Tool-python/tool-kamei.py
@@ -29,7 +29,7 @@ pd.set_option('display.width', 600)  #数値を変更すると改行位置を変
 ####
 #### Outline explanation
 ####   Read "T" file, and creates different Pseudonym IDs each of every month.
-####   It can make some files, 
+####   It can make some files.
 ####         A(T) : IDs changes to Pseudonym IDs.
 ####         S    : Permuted Anonymized data without "DEL".
 ####         R    : Correspondence list of Real ID.
@@ -41,11 +41,12 @@ pd.set_option('display.width', 600)  #数値を変更すると改行位置を変
 
 
 
-###  ファイルを読み込む
-FILE1  = argv[1]
-TRANS_MARK        = pd.read_csv(r"%s" % FILE1  , header=None , dtype={ 0:'S', 1:'S', 2:'S', 3:'S', 4:'S', 5:'S', 6:'S'}   )
+FILE1      = argv[1]
+TRANS_MARK = pd.read_csv(r"%s" % FILE1  , header = None , dtype = { 0:'S', 1:'S', 2:'S', 3:'S', 4:'S', 5:'S', 6:'S'}   )
 
-## 期間ごとに，仮名IDの先頭文字に1月＝A，2月＝B,, の順にマークを付ける．
+## For each month, mark the first character of the pseudonymized ID with a letter. Expl: January = A, February = B
+## The first character of the pseudonimized ID will be A if it buy something in Juanary.
+## All the charachters stend for the month in the year : Jua -> A, Feb -> B, etc...
 DIV_HEAD  = (
 "0",
 "A",
@@ -65,6 +66,11 @@ DIV_HEAD  = (
 ## IDをそのまま仮名化しないように，後ろに文字を付与してハッシュ化する（見えない）
 ## hashlib.md5(元ID ＋ DIV_APPEND[int(月)])  → 1月： A+md5(ID+JA)
 ## この値は利用者が個々に変更すること．
+### We will append a character to the back of it so that it will not be converted to a pseudonym as it is (not visible)
+### hashlib.md 5 (original ID + DIV_APPEND [int (month)]) → January: A + md 5 ID + JA)
+### This value should be changed individually by the user.
+
+## Basicaly represent the ending of the pseudonimized ID in function of the month
 DIV_APPEND  = (
 "0",
 "JA",
@@ -81,96 +87,59 @@ DIV_APPEND  = (
 "DE"
 )
 
-
-
-
 NEW_CODE = []
 
+## Create pseudonymes for data T given in parameters
+# For all rows in T
 for i in range(len( TRANS_MARK[0] )):
-#for i in range(100):
+    # Check if DEL (For T_alpha)
 	if  TRANS_MARK[0][i]  == "DEL" :
 		NEW_CODE.append( "DEL" )
 	else:
-		DIV_LINE = str(TRANS_MARK[2][i]).split("/") 
+        # Get Month of transaction
+		DIV_LINE = str(TRANS_MARK[2][i]).split("/")
 		DIV_CODE = int ( DIV_LINE[1] )
-	
-		## Pseudonym IDs  : 仮名が解るように加工する．システムは通ります．
+
+        # Create the second part of pseudo : ID + Month
 		PsuIDS =  str( str( TRANS_MARK[0][i] ) + str(DIV_APPEND[DIV_CODE]) )
-		## Encrypt IDs (!MD5) ：仮名を暗号化．しかしMD5なので脆弱性あり．
-		#PsuIDS = hashlib.md5( str( str( TRANS_MARK[0][i] ) + str(DIV_APPEND[DIV_CODE]) ).encode('utf-8') ).hexdigest()
-		
+
+        # Append the all pseudonym too a list of code Letter + ID + Month
 		NEW_CODE.append( str(DIV_HEAD[DIV_CODE]) + str(PsuIDS) )
 
+# Affect the Pseudo to T in column PsuID
 TRANS_MARK['PsuID'] = NEW_CODE
 FILENAME =  datetime.now().strftime("%H%M%S")
 
 
-
-
-
-
-
-
-
-## A(T) ：行番号を変えないで出力する→これを匿名化フェイズでは提出する
+## Write the AT file with the pseudonymes created above.
 TRANS_MARK.to_csv( 'AT_%s.csv' % FILENAME , sep=',', index=False, header=False, columns=['PsuID',1,2,3,4,5,6])
 
-
-
-
-
-
-
-
-
-
-## S ：DEL行を除いて，行番号をランダムに変えたものをSとする．これは再識別フェイズに他ユーザに配布される．
-TRANS_MARK1 =  TRANS_MARK[TRANS_MARK[0] != "DEL" ] 
+# Create File S with DEL row removed and lines shuffled.
+TRANS_MARK1 =  TRANS_MARK[TRANS_MARK[0] != "DEL" ]
 TRANS_MARK1 =  TRANS_MARK1.reindex(np.random.permutation(TRANS_MARK1.index))
 TRANS_MARK1.to_csv( 'S_%s.csv' % FILENAME , sep=',', index=False, header=False, columns=['PsuID',1,2,3,4,5,6])
 
 
-
-
-
-
-
-
-
-## R ：Sにおける，各行の仮名化される前の本当のID．これが解ると再識別されてしまう．
+## Create file R containing the ID <--> PsuID correspondance with S
 TRANS_MARK1.to_csv( 'R_%s.csv' % FILENAME , sep=',', index=False, header=False, columns=[0])
-
-
-
-
-
-
-
 
 P_ROW     = TRANS_MARK1.index
 P_ROWLIST = []
 for i in range(len( TRANS_MARK1.index )):
-	P_ROWLIST.append( P_ROW[i] + 1 ) 
+	P_ROWLIST.append( P_ROW[i] + 1 )
 
-## P ：A(T)→Sに変換したときの行番号データ．これが解ると再識別されてしまう．
+## Create file P : the order of the shuffle table S.
 TRANS_MARK1["P"] = P_ROWLIST
 TRANS_MARK1.to_csv( 'P_%s.csv' % FILENAME , sep=',', index=False, header=False, columns=["P"])
-
-
-
-
-
 
 print(  TRANS_MARK1 )
 print(  TRANS_MARK1.loc[:,[0,'PsuID']]  )
 
-
-
-
-##行列の数を確認
+## YY -> size of database (#rows)
+## XX -> size of attributes (#columns)
 YY,XX = TRANS_MARK.shape
-print ( "S_%s.csv は %s 行 %s 列" % (FILENAME, YY,XX) ) 
-print ( "経過時間：%s 秒" % round( time()-START  ,2 ) )
-print ( "処理終了：%s" % datetime.now().strftime("%H:%M:%S") )
+print ( "T_%s.csv in %s rows %s columns" % (FILENAME, YY,XX) )
+print ( "execution time: %s" % round( time()-START  ,2 ) )
+print ( "time now: %s" % datetime.now().strftime("%H:%M:%S") )
 
 
