1ea8518c798c33c1030df2053a854b3b58e54979
Antoine Laurent <antoine@famille-laurent.com>
2018-05-18 17:37:47 -0400

    tsne: function to compute and save t-sne calcul

--- scripts/python/dataset_creation/tsne_verification.py
+++ scripts/python/dataset_creation/tsne_verification.py
@@ -9,9 +9,22 @@ Description: Script to generate several t-sne to understand the dispertion in th
 import sys
 import getopt
 
-from sklearn.manifold import TSE
+import progressbar
 import pandas as pd
+import matplotlib.pyplot as plt
+from sklearn.manifold import TSNE
 
+PERPLEXITY = [10, 20, 30, 50]
+
+def save_fig_tsne(data, save_path, perplexity=20):
+    """ calculate and save figure of t-sne for a certain perplexity
+
+    :perplexity: the perplexity parameter in t-sne
+    """
+    tsne = TSNE(perplexity=perplexity).fit_transform(data)
+    plt.clf()
+    plt.scatter(tsne[:, 0], tsne[:, 1])
+    plt.savefig('{}/tsne_p{}.png'.format(save_path, perplexity))
 
 def main():
     """main function
@@ -23,7 +36,7 @@ def main():
     try:
         # Short option syntax: "hv:"
         # Long option syntax: "help" or "verbose="
-        opts, args = getopt.getopt(sys.argv[1:], "hp:s:", ["help", "path", "save_path"])
+        opts, _ = getopt.getopt(sys.argv[1:], "hp:s:", ["help", "path", "save_path"])
 
     except getopt.GetoptError as err:
         # Print debug info
@@ -38,6 +51,14 @@ def main():
         elif option in ("-s", "--save_path"):
             save_path = argument
 
+    data = pd.read_csv(path)
+    data = data.drop("id_user", axis=1)
+    data = data.drop("id_items_usr", axis=1)
+
+    pbar = progressbar.ProgressBar()
+
+    for perplexity in pbar(PERPLEXITY):
+        save_fig_tsne(data, save_path, perplexity)
 
 if __name__ == "__main__":
     main()
