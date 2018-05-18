798eac7cb8e5164d990bbd1fe23ffc48d64d4c5c
Antoine Laurent <antoine@famille-laurent.com>
2018-05-18 16:21:24 -0400

    extract_transaction: function to transform date
    transform date into the date format used in PWSCup

--- scripts/python/dataset_creation/extract_transaction.py
+++ scripts/python/dataset_creation/extract_transaction.py
@@ -11,12 +11,15 @@ transaction and user with low number of transaction).
 
 import sys
 import getopt
+import datetime
 
 import pandas as pd
 
 C_MIN = 5
 C_MAX = 500
 COL = {"id_user":"CustomerID", "date":"InvoiceDate", "hours":"heure"}
+NEW_COLS = ['InvoiceNo', 'id_item', 'Description', 'qte',\
+            'date', 'price', 'id_user', 'Country', 'heure']
 
 def get_id_with_count_between(data, c_min, c_max):
     """ recover all id of user that have a transaction count between c_min and c_max in data
@@ -38,6 +41,41 @@ def get_id_with_count_between(data, c_min, c_max):
     inter = set(inf_c_max).intersection(set(sup_c_min))
 
     return list(inter)
+
+def transform_date_pwscup_way(data):
+    """Transform date to mach the date format from PWSCup to be able to use the script efficiently.
+
+    :data: the DataFrame to transform
+
+    :returns: the DataFrame transformed
+
+    """
+    data[COL["date"]] = pd.to_datetime(data[COL["date"]], format='%Y-%m-%d %H:%M:%S')
+
+    def get_date(date):
+        """Get date from timestamp
+        """
+        return date.date()
+    def get_time(date):
+        """Get hours:minute:sec from timestamp
+        """
+        return date.time()
+    def format_date(date):
+        """Format date to match string
+        """
+        return date.strftime('%Y/%m/%d')
+    def format_time(hour):
+        """Format time to match string
+        """
+        return hour.strftime('%H:%M')
+
+    data.loc[:, COL['hours']] = data[COL['date']].apply(lambda x: get_time(x))
+    data.loc[:, COL['date']] = data[COL['date']].apply(lambda x: get_date(x))
+
+    data.loc[:, COL['date']] = data[COL['date']].apply(lambda x: format_date(x))
+    data.loc[:, COL['hours']] = data[COL['hours']].apply(lambda x: format_time(x))
+
+    return data
 
 def main():
     """Main function
@@ -72,6 +110,16 @@ def main():
     #recover the list of user of interest (uoi)
     list_uoi = get_id_with_count_between(data, C_MIN, C_MAX)
 
+    data = data.loc[data['CustomerID'].isin(list_uoi)]
+    data = data.reset_index(drop=True)
+
+    data = transform_date_pwscup_way(data)
+
+    #change columns for extract_profile scripts
+    data.columns = NEW_COLS
+
     data.to_csv("{}/data_extracted_{}-{}_traj.csv".format(save_path, C_MIN, C_MAX), index=False)
+
+
 if __name__ == "__main__":
     main()
