5486b5700cff50893932369db346a07c87992075
Antoine Laurent <antoine@famille-laurent.com>
2018-05-22 16:54:05 -0400

    evaluator: skeleton of class done

--- source/evaluation/evaluator.py
+++ source/evaluation/evaluator.py
@@ -1,41 +1,71 @@
 import pandas as pd
 import numpy as np
 
-class ExampleEvaluator:
-  def __init__(self, answer_file_path, round=0):
-    """
-    `round` : Holds the round for which the evaluation is being done. 
-    can be 0, 1, 2...upto the number of rounds the challenge has.
-    Different rounds will mostly have different ground truth files.
-    """
-    self.answer_file_path = answer_file_path
-    self.round = round
+class Evaluator(object):
 
-  def _evaluate(self, client_payload, _context={}):
-    """
-    `client_payload` will be a dict with (atleast) the following keys :
-      - submission_file_path : local file path of the submitted file
-    """
-    submission_file_path = client_payload["submission_file_path"]
-    submission = pd.read_csv(submission_file_path)
-    # Or your preferred way to read your submission
+    """ Evaluate performance of re-identification attack or anonymisation depending on the round """
 
-    """
-    Do something with your submitted file to come up
-    with a score and a secondary score.
+    def __init__(self, answer_file_path, round=0):
+        """
+        `round` : Holds the round for which the evaluation is being done.
+        can be 0, 1, 2...upto the number of rounds the challenge has.
+        Different rounds will mostly have different ground truth files.
+        """
+        self.answer_file_path = answer_file_path
+        self.round = round
 
-    if you want to report back an error to the user,
-    then you can simply do :
-      `raise Exception("YOUR-CUSTOM-ERROR")`
+    def _control_file_format(self, arg1):
+        """TODO: Docstring for _control_file_format.
 
-     You are encouraged to add as many validations as possible
-     to provide meaningful feedback to your users
-    """
-    _result_object = {
-        "score": np.random.random(),
-        "score_secondary" : np.random.random()
-    }
-    return _result_object
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def _anonymisation_evaluation(self, arg1):
+        """TODO: Docstring for _anonymisation_evaluation.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def reidentificaton_evaluation(self, arg1):
+        """TODO: Docstring for reidentificaton_evaluation.
+
+        :arg1: TODO
+        :returns: TODO
+
+        """
+        pass
+
+    def _evaluate(self, client_payload, _context={}):
+        """
+        `client_payload` will be a dict with (atleast) the following keys :
+          - submission_file_path : local file path of the submitted file
+        """
+        submission_file_path = client_payload["submission_file_path"]
+        submission = pd.read_csv(submission_file_path)
+        # Or your preferred way to read your submission
+
+        """
+        Do something with your submitted file to come up
+        with a score and a secondary score.
+
+        if you want to report back an error to the user,
+        then you can simply do :
+          `raise Exception("YOUR-CUSTOM-ERROR")`
+
+         You are encouraged to add as many validations as possible
+         to provide meaningful feedback to your users
+        """
+        _result_object = {
+                "score": np.random.random(),
+                "score_secondary" : np.random.random()
+                }
+        return _result_object
 
 if __name__ == "__main__":
     # Lets assume the the ground_truth is a CSV file
@@ -47,7 +77,7 @@ if __name__ == "__main__":
     # Instaiate a dummy context
     _context = {}
     # Instantiate an evaluator
-    crowdai_evaluator = ExampleEvaluator(answer_file_path)
+    crowdai_evaluator = Evaluator(answer_file_path)
     # Evaluate
     result = crowdai_evaluator._evaluate(_client_payload, _context)
     print(result)
