90dcca4ca68f82d5789f9988723c90e580ae9bd5
Antoine Laurent <antoine@famille-laurent.com>
2018-06-11 16:15:50 -0400

    metrics, e3: fix copy/paste bugs in e3 metrics
    a lot of copy/paste problems (with anon_colab_fi and gt_.. one) resolved. Also
    change the execution order of methods

--- source/evaluation/metrics.py
+++ source/evaluation/metrics.py
@@ -680,17 +680,24 @@ class UtilityMetrics(Metrics):
 
         return dist
 
-    def e3_metric(self):
+    def e3_metric(self, param_k=100):
         """TODO: Docstring for e3_metric.
         :returns: TODO
 
         """
-
         # Processing of Ground Truth Database
         gt_colab_fi = CollaborativeFiltering(self._ground_truth)
 
         gt_colab_fi.preprocessing_data(max_qty_score=True, top_k=True)
-        gt_tok_k_ids = gt_colab_fi.make_topk_item_list(k=100)
+        gt_tok_k_ids = gt_colab_fi.make_topk_item_list(k=param_k)
+
+        anon_colab_fi = CollaborativeFiltering(self._anon_trans,\
+                item_table=gt_colab_fi.item_table)
+
+        anon_colab_fi.preprocessing_data(max_qty_score=False, top_k=True)
+        anon_tok_k_ids = anon_colab_fi.make_topk_item_list(k=param_k)
+
+        gt_colab_fi = CollaborativeFiltering(self._ground_truth)
         gt_colab_fi.preprocessing_data(max_qty_score=True, top_k=True, top_k_ids=gt_tok_k_ids)
 
         item_item_dic1 = gt_colab_fi.calc_item2item_dic()
@@ -699,15 +706,13 @@ class UtilityMetrics(Metrics):
         anon_colab_fi = CollaborativeFiltering(self._anon_trans,\
                 item_table=gt_colab_fi.item_table)
 
-        anon_colab_fi.preprocessing_data(max_qty_score=False, top_k=True)
-        anon_tok_k_ids = gt_colab_fi.make_topk_item_list(k=100)
         anon_colab_fi.preprocessing_data(max_qty_score=True, top_k=True, top_k_ids=gt_tok_k_ids)
 
         item_item_dic2 = anon_colab_fi.calc_item2item_dic()
 
         # Calcul of the distance
         dist = []
-        dist.append(len(set(anon_tok_k_ids).difference(set(anon_tok_k_ids))))
+        dist.append(len(set(gt_tok_k_ids).difference(set(anon_tok_k_ids))) / param_k)
         dist.append(self._calc_sim_mat_dist(item_item_dic1, item_item_dic2))
 
         return dist
