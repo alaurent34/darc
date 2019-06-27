42ebb41c284e0977ffc71345cb99ffba76a43dba
Antoine Laurent <antoine@famille-laurent.com>
2019-06-27 16:17:23 -0400

    config: add r2 storage path

--- config.py
+++ config.py
@@ -6,6 +6,8 @@ class Config:
     REDIS_PORT=os.environ.get("REDIS_PORT", 6379)
     REDIS_PASSWORD=os.environ.get("REDIS_PASSWORD", False)
 
+    ROUND2_STORAGE = "data/testing_files/r2_storage/"
+
     GROUND_TRUTH = "data/ground_truth.csv"
     R1_SUBMISSION_FILE = "data/example_files/submission_DEL.csv"
     R2_SUBMISSION_FILE = "./data/example_files/F_a_attempt_2.tar"
