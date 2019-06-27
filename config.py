c936c246409a193f20f8758f65a3b8c463e6dfc6
Antoine Laurent <antoine@famille-laurent.com>
2019-06-27 16:16:59 -0400

    config: rm owncloud var

--- config.py
+++ config.py
@@ -6,10 +6,6 @@ class Config:
     REDIS_PORT=os.environ.get("REDIS_PORT", 6379)
     REDIS_PASSWORD=os.environ.get("REDIS_PASSWORD", False)
 
-    OC_HOST=os.environ.get("OC_HOST", "localhost")
-    OC_USR=os.environ.get("OC_USR", "test")
-    OC_PASSWORD=os.environ.get("OC_PASSWORD", "password")
-
     GROUND_TRUTH = "data/ground_truth.csv"
     R1_SUBMISSION_FILE = "data/example_files/submission_DEL.csv"
     R2_SUBMISSION_FILE = "./data/example_files/F_a_attempt_2.tar"
